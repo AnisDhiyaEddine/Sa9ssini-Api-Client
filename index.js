ef756fd74fcfb266064abd8d3b6dc35f9fc30854
ANISDHIYAEDDINE <a.boudiaf@esi-sba.dz>
2020-05-02 22:10:32 +0100

    setUp

---
+++ index.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const regeneratorRuntime = require("regenerator-runtime");
+const url = 'http://localhost:3000'
+
+console.log("up and runing")
+
+
+let user = {
+    userName: "Test UserName",
+    email: "test@gmail.com",
+    password: "testHashed"
+}
+
+
+ 
+const main = async()=>{ 
+    try { 
+        
+    } catch (error) {
+        console.log(error) 
+    } 
+} 
+main()
+
+      
